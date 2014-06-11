73151efa20e6430360937d81e282afcf4d2f9f32
Vadim Zaytsev <zaic101@gmail.com>
2014-06-11 15:01:09 +0700

    Add C++ blank file.

---
+++ blank.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef ILIKEGENTOO
+void E(){}template<class A,class...B>void E(A $,B..._){cerr<<' '<<$;E(_...);}
+# define E($...) E(#$,'=',$,'\n')
+#else
+# define E(x...)
+#endif
+#define Sz(x) (int((x).size()))
+#define All(x) begin(x),end(x)
+
+typedef double flt;
+typedef long long int64;
+typedef pair<int, int> pii;
+
+const int inf = 0x3f3f3f3f;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    return 0;
+}
