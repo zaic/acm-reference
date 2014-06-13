d9b42b07e328b9522d238ff38f6c71b44d1646ee
Vadim Zaytsev <zaic101@gmail.com>
2014-06-13 16:18:10 +0700

    C++11

--- bipartite.cpp
+++ bipartite.cpp
@@ -1,14 +1,13 @@
 // ********************* Match in bipartite graph **********************
-const int maxn = 1 << 10;
+const int maxn = 1010;
 int who[maxn];
-char was[maxn];
+bool was[maxn];
 vector<int> g[maxn];
 
 bool go(int v){
     if (was[v]) return false;
     was[v] = 1;
-    for (int i = 0; i < g[v].size(); ++i) {
-        int p = g[v][i];
+    for (int p : g[v]) {
         if (who[p] < 0 || go(who[p])) {
             who[p] = v;
             return true;
@@ -21,7 +20,7 @@ int max_match(int n) {
     memset(who, -1, sizeof(int) * n);
     int res = 0;
     for (int i = 0; i < n; ++i) {
-        memset(was, 0, sizeof(char) * n);
+        memset(was, 0, sizeof(bool) * n);
         if (go(i)) ++res;
     }
     return res;
