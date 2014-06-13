bfcdeb79b0223f35b9c6978d8e5f6ddf3c11e610
Vadim Zaytsev <zaic101@gmail.com>
2014-06-13 16:06:40 +0700

    Update Hungrian algorithm to rectangular matrix and non-integer weights.

--- hungarian.cpp
+++ hungarian.cpp
@@ -1,35 +1,41 @@
-// ************************ Hungarian algorithm ************************
-const int inf = 0x3f3f3f3f;
-int u[maxn], v[maxn], p[maxn], prev[maxn], minv[maxn], nw[maxn];
-int c[maxn][maxn];
-int n;
+// ************************ Hungarian algorithm (min cost) ************************
+const double inf = 1e20;
+const int maxn = 1010, maxm = 2020;
+int prv[maxm], nw[maxm];
+double u[maxn], v[maxm], minv[maxm];
 
-int min_cost_assignement() {
-	memset(u, 0, sizeof(int) * (n + 1));
-	memset(v, 0, sizeof(int) * (n + 1));
-	memset(p, 0, sizeof(int) * (n + 1));
+int n, m; // number of rows and cols
+double c[maxn][maxm]; // input matrix
+int p[maxm]; // answer: (p[i]-1) is a row index for column i
+
+double min_cost_assignement() {
+	memset(u, 0, sizeof(double) * (n + 1));
+	memset(v, 0, sizeof(double) * (m + 1));
+	memset(p, 0, sizeof(int) * (m + 1));
+    memset(prv, 0, sizeof(int) * (m + 1));
 	for (int i = 1; i <= n; ++i) {
 		int j0 = 0, j1 = -1;
 		p[0] = i;
-		memset(minv, 0x3f, sizeof(int) * (n + 1));
+        for (int j = 0; j <= m; ++j) minv[j] = inf;
 		minv[0] = 0;
-		memset(nw, 0, sizeof(int) * (n + 1));
+		memset(nw, 0, sizeof(int) * (m + 1));
 		do {
 			nw[j0] = 1;
-			int i0 = p[j0], del = inf;
-			for (int j = 1; j <= n; ++j) if (nw[j] == 0) {
-				int cc = c[i0 - 1][j - 1] - u[i0] - v[j];
+			int i0 = p[j0];
+            double del = inf;
+			for (int j = 1; j <= m; ++j) if (nw[j] == 0) {
+				double cc = c[i0 - 1][j - 1] - u[i0] - v[j];
 				if (cc < minv[j]) {
 					minv[j] = cc;
-					prev[j] = j0;
+					prv[j] = j0;
 				}
 				if (minv[j] < del) {
 					del = minv[j];
 					j1 = j;
 				}
 			}
-			for (int j = 0; j <= n; ++j)
-				if (nw[j] == 1) {
+			for (int j = 0; j <= m; ++j)
+				if (nw[j]) {
 					u[p[j]] += del;
 					v[j] -= del;
 				} else
@@ -37,12 +43,12 @@ int min_cost_assignement() {
 			j0 = j1;
 		} while (p[j0] != 0);
 		do {
-			j1 = prev[j0];
+			j1 = prv[j0];
 			p[j0] = p[j1];
 			j0 = j1;
-		} while (j0 != 0);
+		} while (j0);
 	}
-	int ans = 0;
-	for (int i = 1; i <= n; ++i) ans += c[p[i] - 1][i - 1];
+	double ans = 0;
+	for (int i = 1; i <= m; ++i) if (p[i] > 0) ans += c[p[i] - 1][i - 1];
 	return ans;
 }
